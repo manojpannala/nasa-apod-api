9693753f33a810591696c2e8d873982590d6cece
manojpannala <manojpannala@outlook.com>
2021-02-12 18:36:25 +0200

    home.js updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home">
-      <Link className="home-link" to="/nasaphoto">See into the stars!</Link>
+      <Link className="home-link" to="/nasaphoto">NASA Astronomy POD</Link>
     </div>
   );
 }
